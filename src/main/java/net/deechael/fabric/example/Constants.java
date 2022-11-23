d2621523f391bb2374ae68f87481773c558d04c3
DeeChael <DeeChael@outlook.com>
2022-11-23 09:00:20 +0800

    init

---
+++ src/main/java/net/deechael/fabric/example/Constants.java
@@ -0,0 +1,13 @@
+package net.deechael.fabric.example;
+
+public final class Constants {
+	
+	public static final Logger LOGGER = LoggerFactory.getLogger(Constants.MOD_ID);
+
+    public final static String MOD_ID = "example";
+
+    public final static String MOD_NAME = "Example Mod";
+
+    private Constants() {}
+	
+}
