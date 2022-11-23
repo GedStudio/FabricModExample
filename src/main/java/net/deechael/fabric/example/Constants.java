db473bac84aa71db7ba3e532ed2ba55ddee1dd2a
DeeChael <DeeChael@outlook.com>
2022-11-23 09:06:47 +0800

    fix: classes missed when import

--- src/main/java/net/deechael/fabric/example/Constants.java
+++ src/main/java/net/deechael/fabric/example/Constants.java
@@ -1,5 +1,8 @@
 package net.deechael.fabric.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public final class Constants {
 	
 	public static final Logger LOGGER = LoggerFactory.getLogger(Constants.MOD_ID);
